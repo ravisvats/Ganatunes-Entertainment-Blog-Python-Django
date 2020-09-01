0d0a204c7905ed81b853955c51f0ba5dc824e179
ravisvats <ravisvats@gmail.com>
2020-09-01 18:38:38 +0530

    change

--- ganatunes/blog/models.py
+++ ganatunes/blog/models.py
@@ -4,28 +4,7 @@ from django.urls import reverse
 from datetime import datetime, date
 from ckeditor.fields import RichTextField
 
-class Category(models.Model):
-    name = models.CharField(max_length=250)
 
-    def __str__(self):
-        return self.name
-    def get_absolute_url(self):
-        #return reverse('article_detail', args=(str(self.id)))
-        return reverse('home')
-
-class Profile(models.Model):
-    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
-    bio = models.TextField()
-    profile_pic = models.ImageField(null=True, blank=True, upload_to="images/profile/")
-    website_url = models.TextField(max_length=250, null=True, blank=True,)
-    facebook_url = models.TextField(max_length=250, null=True, blank=True, )
-    twitter_url = models.TextField(max_length=250, null=True, blank=True, )
-    instagram_url = models.TextField(max_length=250, null=True, blank=True, )
-    pinterest_url = models.TextField(max_length=250, null=True, blank=True, )
-
-
-    def __str__(self):
-        return str(self.user)
 
     def get_absolute_url(self):
             return reverse('home')
